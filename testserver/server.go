703bbba8121874dd2d995d19c4ee1aaf38707e9b
Igor Mihalik <igor.mihalik@gmail.com>
2012-11-26 11:38:12 +0000

    test server update to handle wrong URLs from sockjs protocol tests

--- testserver/server.go
+++ testserver/server.go
@@ -4,6 +4,7 @@ import (
 	"github.com/igm/sockjs-go/sockjs"
 	"log"
 	"net/http"
+	"path"
 )
 
 func main() {
@@ -18,7 +19,6 @@ func main() {
 			Prefix:        "/echo",
 		},
 	})
-
 	http.Handle("/disabled_websocket_echo/", sockjs.SockJSHandler{
 		Handler: SockJSHandler,
 		Config: sockjs.Config{
@@ -40,7 +40,7 @@ func main() {
 	})
 
 	http.Handle("/", http.FileServer(http.Dir("./www")))
-	err := http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe(":8080", &TestServer{})
 	log.Fatal(err)
 }
 
@@ -59,4 +59,34 @@ func SockJSHandler(session *sockjs.SockJsConn) {
 	}
 
 	log.Println("session closed")
+}
+
+// Stolen from http package
+func cleanPath(p string) string {
+	if p == "" {
+		return "/"
+	}
+	if p[0] != '/' {
+		p = "/" + p
+	}
+	np := path.Clean(p)
+	// path.Clean removes trailing slash except for root;
+	// put the trailing slash back if necessary.
+	if p[len(p)-1] == '/' && np != "/" {
+		np += "/"
+	}
+	return np
+}
+
+type TestServer struct {
+	*http.ServeMux
+}
+
+func (m *TestServer) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	// To get the sockjs-protocol tests to work, barf if the path is not already clean.
+	if req.URL.Path != cleanPath(req.URL.Path) {
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+	http.DefaultServeMux.ServeHTTP(w, req)
 }
