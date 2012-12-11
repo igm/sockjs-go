ca5c2548b2e6d1806dd1e75eb7aed89458fcec66
Igor Mihalik <igor.mihalik@gmail.com>
2012-12-11 20:19:23 +0000

    Update sockjs/doc.go

--- sockjs/doc.go
+++ sockjs/doc.go
@@ -1,6 +1,6 @@
 /*
 Package gosockjs is an concurrent safe implementation of a SockJS server. The core design principles are described
-here http://blog.igormihalik.com/sockjs-for-go.html and source code can be found on github https://github.com/igm/sockjs-go
+here http://blog.igormihalik.com/2012/12/sockjs-for-go.html and source code can be found on github https://github.com/igm/sockjs-go
 
 */
 package sockjs
