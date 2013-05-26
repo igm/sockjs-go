bb4c150b7440621661dd886def302c14d0cecd1b
Grant Thomas <jgrantthomas@gmail.com>
2013-05-26 12:15:25 -0700

    split the message body

--- sockjs/multiplex.go
+++ sockjs/multiplex.go
@@ -1,6 +1,7 @@
 package sockjs
 
 import (
+	"strings"
 	"log"
 	"container/list"
 	)
@@ -14,6 +15,24 @@ func (this ConnectionMultiplexer) Handle(conn Conn) {
 	for {
 		if msg, err := conn.ReadMessage(); err == nil {
 			// add client to a channel
+			parts := strings.Split(bytesToString(msg), ",")
+			if (len(parts) >= 3) {
+				var msg_type string
+				var msg_channel string
+				var msg_payload string
+				msg_type, parts = parts[len(parts)-1], parts[:len(parts) - 1]
+				msg_channel, parts = parts[len(parts)-1], parts[:len(parts) - 1]
+				msg_payload = strings.Join(parts,"")
+				if msg_type == 'sub' {
+					if 
+				} else if msg_type == 'uns' {
+					
+				} else if msg_type == 'msg' {
+					
+				} else {
+					
+				}
+			}
 			// create new channel (if it doesn't exists)
 			// broadcast message to channel
 			// remove client from channel
@@ -44,4 +63,15 @@ func (this *ConnectionMultiplexer) RegisterChannel(channelName string) {
 }
 
 func (this *ConnectionMultiplexer) Broadcast(channelName string, message string) {
+}
+
+func bytesToString(bytes []byte) string {
+	n := -1
+	for i, b := range bytes {
+		if b == 0 {
+			break
+		}
+		n = i
+	}
+	return string(bytes[:n+1])
 }
