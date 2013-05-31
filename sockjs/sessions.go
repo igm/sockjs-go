7f310b61fda162822d5006e0830dbe8d82d459ae
Igor Mihalik <igor.mihalik@gmail.com>
2013-05-31 15:07:03 +0100

    Let's use sync facilities instead of routines. It does not make much sence to sequence reads, so RLock is enough. Also there's no performance benefit of using routines instead of Locks.

--- sockjs/sessions.go
+++ sockjs/sessions.go
@@ -1,52 +1,42 @@
 package sockjs
 
+import (
+	"sync"
+)
+
 type connections struct {
 	connections map[string]*conn
-	req         chan func()
+	mu          sync.RWMutex
 }
 
 type connFactory func() *conn
 
 func newConnections() connections {
-	connections := connections{
+	return connections{
 		connections: make(map[string]*conn),
-		req:         make(chan func()),
 	}
-	// go routine to perform concurrent-safe operations with connections map
-	go func() {
-		for r := range connections.req {
-			r()
-		}
-	}()
-	return connections
 }
 
 func (this *connections) get(sessid string) (conn *conn, exists bool) {
-	resp := make(chan bool)
-	this.req <- func() {
-		conn, exists = this.connections[sessid]
-		resp <- true
-	}
-	<-resp
+	this.mu.RLock()
+	defer this.mu.RUnlock()
+	conn, exists = this.connections[sessid]
 	return
 }
 
 func (this *connections) getOrCreate(sessid string, f connFactory) (conn *conn, exists bool) {
-	resp := make(chan bool)
-	this.req <- func() {
-		conn, exists = this.connections[sessid]
-		if !exists {
-			this.connections[sessid] = f()
-			conn = this.connections[sessid]
-		}
-		resp <- true
+	this.mu.Lock()
+	defer this.mu.Unlock()
+	conn, exists = this.connections[sessid]
+	if !exists {
+		this.connections[sessid] = f()
+		conn = this.connections[sessid]
 	}
-	<-resp
 	return
 }
 
 func (this *connections) delete(sessid string) {
-	this.req <- func() {
-		delete(this.connections, sessid)
-	}
+	this.mu.Lock()
+	defer this.mu.Unlock()
+	delete(this.connections, sessid)
 }
